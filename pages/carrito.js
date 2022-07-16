73a8260428ad6413e93627273baeb0cd18ff13e2
jeanmo333 <jeanmo333@gmail.com>
2022-07-15 23:11:54 -0400

    commit

--- pages/carrito.js
+++ pages/carrito.js
@@ -21,65 +21,65 @@ const Carrito = ({ carrito, actualizarCantidad, eliminarProducto }) => {
 
       <main className={`${styles.contenido} contenedor`}>
         <div className={styles.carrito}>
-          <h2>Articulos</h2>
-
-          {carrito.length === 0
-            ? "carrito vacio"
-            : carrito.map((producto) => (
-                <div key={producto._id} className={styles.producto}>
-                  <div>
-                    <Image
-                      layout="responsive"
-                      width={250}
-                      height={500}
-                      src={producto.imagen}
-                      alt={producto.nombre}
-                    />
-                  </div>
-
-                  <div>
-                    <p className={styles.nombre}>{producto.nombre}</p>
-                    <div className={styles.cantidad}>
-                      <p> Cantidad:</p>
-                      <select
-                        value={producto.cantidad}
-                        className={styles.select}
-                        onChange={(e) =>
-                          actualizarCantidad({
-                            cantidad: e.target.value,
-                            _id: producto._id,
-                          })
-                        }
-                      >
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                      </select>
-                    </div>
-
-                    <p className={styles.precio}>
-                      <span>{formatearCantidad(producto.precio)}</span>
-                    </p>
-
-                    <p className={styles.subTotal}>
-                      Subtotal :{" "}
-                      <span>
-                        {formatearCantidad(producto.precio * producto.cantidad)}
-                      </span>
-                    </p>
-                  </div>
-
-                  <button
-                    type="button"
-                    className={styles.eliminar}
-                    onClick={() => eliminarProducto(producto._id)}
-                  >
-                    X
-                  </button>
+          {carrito.length === 0 ? (
+            <p className={styles.vacio}>carrito vacio</p>
+          ) : (
+            carrito.map((producto) => (
+              <div key={producto._id} className={styles.producto}>
+                <div>
+                  <Image
+                    layout="responsive"
+                    width={250}
+                    height={500}
+                    src={producto.imagen}
+                    alt={producto.nombre}
+                  />
                 </div>
-              ))}
+
+                <div>
+                  <p className={styles.nombre}>{producto.nombre}</p>
+                  <div className={styles.cantidad}>
+                    <p> Cantidad:</p>
+                    <select
+                      value={producto.cantidad}
+                      className={styles.select}
+                      onChange={(e) =>
+                        actualizarCantidad({
+                          cantidad: e.target.value,
+                          _id: producto._id,
+                        })
+                      }
+                    >
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                    </select>
+                  </div>
+
+                  <p className={styles.precio}>
+                    <span>{formatearCantidad(producto.precio)}</span>
+                  </p>
+
+                  <p className={styles.subTotal}>
+                    Subtotal :{" "}
+                    <span>
+                      {formatearCantidad(producto.precio * producto.cantidad)}
+                    </span>
+                  </p>
+                </div>
+
+                <button
+                  type="button"
+                  className={styles.eliminar}
+                  onClick={() => eliminarProducto(producto._id)}
+                >
+                  X
+                </button>
+              </div>
+            ))
+          )}
         </div>
 
         <div className={styles.resumen}>
@@ -89,7 +89,7 @@ const Carrito = ({ carrito, actualizarCantidad, eliminarProducto }) => {
               <p>Total a pagar: {formatearCantidad(total)}</p>
             </>
           ) : (
-            <p>Carrito vacio</p>
+            <p>Resumen va Aparecer Aqui</p>
           )}
         </div>
       </main>
